25ca1918877f4ff9657787b90be1899c85c41f51
Aman Adhikari <amanadhikari2@gmail.com>
2019-12-20 22:40:48 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ApplicationProvider, Layout, Text, IconRegistry  } from '@ui-kitten/components';
+import { mapping, light as lightTheme } from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { default as appTheme } from './custom-theme.json'; // <-- Import app theme
+import { AppNavigator } from './components/navigation.component';
+const theme = { ...lightTheme, ...appTheme };
+
+const App = () => (
+  <React.Fragment>
+    <IconRegistry icons={EvaIconsPack}></IconRegistry>
+    <ApplicationProvider mapping={mapping} theme={theme}>
+       <AppNavigator/>
+    </ApplicationProvider>
+  </React.Fragment>
+);
+
+export default App;
---
+++ components/navigation.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './home.component';
+import AccountScreen from './account.component';
+import CartScreen from './cart.component';
+import NearByScreen from './nearby.component';
+import { SafeAreaView } from 'react-navigation';
+import { Button, Divider, Layout,BottomNavigation, BottomNavigationTab,Icon } from '@ui-kitten/components';
+const HomeIcon = (style) => (
+  <Icon {...style} name='home' />
+);
+const AccountIcon = (style) => (
+  <Icon {...style} name='person-outline' />
+);
+const CartIcon = (style) => (
+  <Icon {...style} name='shopping-bag-outline' />
+);
+const NearIcon = (style) => (
+  <Icon {...style} name='compass-outline' />
+);
+export const TabBarComponent = ({ navigation }) => {
+
+ const onSelect = (index) => {
+    const selectedTabRoute = navigation.state.routes[index];
+    navigation.navigate(selectedTabRoute.routeName);
+  };
+
+  return (
+    <SafeAreaView>
+      <BottomNavigation appearance='noIndicator' selectedIndex={navigation.state.index} onSelect={onSelect}>
+        <BottomNavigationTab icon={HomeIcon}/>
+        <BottomNavigationTab icon={NearIcon}/>
+		<BottomNavigationTab icon={CartIcon}/>
+		<BottomNavigationTab icon={AccountIcon}/>
+      </BottomNavigation>
+    </SafeAreaView>
+  );
+};
+const HomeNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  NearBy: NearByScreen,
+  Cart: CartScreen,
+  Account: AccountScreen,
+}, {
+  tabBarComponent:TabBarComponent
+});
+
+export const AppNavigator = createAppContainer(HomeNavigator);
